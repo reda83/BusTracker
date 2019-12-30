3e77d8d200cf9bebec3dc68ae98fef45e1de30ad
Ibrahimbibers <Ibrahimbibers@github.com>
2019-12-30 17:06:58 +0200

    Added User location Permissions
    plus hagat tanya

--- app/src/main/java/com/example/bustracker/MapsActivity.java
+++ app/src/main/java/com/example/bustracker/MapsActivity.java
@@ -1,21 +1,15 @@
 package com.example.bustracker;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -25,7 +19,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -35,8 +28,6 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -46,15 +37,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import android.os.Bundle;
-
-import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     Button button;
-
-    private FusedLocationProviderClient client;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
     private GoogleMap mMap;
     Marker currentMarker = null;
     FirebaseAuth mFirebaseAuth;
@@ -64,17 +51,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     double longit = 31.4985;
     GoogleSignInClient mGoogleSignInClient;
 
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private Boolean mLocationPermissionsGranted = false;
+    private static final float DEFAULT_ZOOM = 15f;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         mFirebaseAuth = FirebaseAuth.getInstance();
-        client=LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationProviderClient =LocationServices.getFusedLocationProviderClient(this);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        getLocationPermission();
 
         button = findViewById(R.id.btn1);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -84,80 +77,134 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
+    private void initMap(){
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        mapFragment.getMapAsync(MapsActivity.this);
+
+    }
+
+    private void getLocationPermission(){
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionsGranted = true;
+                initMap();
+            }else{
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+
+    private void moveCamera(LatLng latLng, float zoom){
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    private void getDeviceLocation(){
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            if(mLocationPermissionsGranted){
+
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Location currentLocation = (Location) task.getResult();
+
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM);
+
+                            //getRouteToMarker(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()));
+
+                        }else{
+                            Toast.makeText(MapsActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }catch (SecurityException e){
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
-
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
         mMap = googleMap;
-//        MarkerOptions marker = new MarkerOptions().position(new LatLng(latit, longit)).title("Hello Maps");
-//        marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.bus));
-//        googleMap.addMarker(marker); how to add customized marker
 
-        mMap.getUiSettings().setMyLocationButtonEnabled(true);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return; //el permissions
-        }
-        mMap.setMyLocationEnabled(true);
-//        DriverTrackMove();
-//        setDriverTrack();
+        if (mLocationPermissionsGranted) {
+            getDeviceLocation();
 
-//        Polyline line = mMap.addPolyline(new PolylineOptions()
-//                .add(new LatLng(29.993058, 31.417643), new LatLng(29.996336, 31.419788),new LatLng(29.996425, 31.419915) ,new LatLng(29.996588, 31.419944))
-//                .width(10)
-//                .color(Color.BLUE)); how to draw polylines
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
 
-        //calling set location every 3 seconds
-        if(account==null){
-            final Handler ha=new Handler();
-            ha.postDelayed(new Runnable() {
+            //        DriverTrackMove();
+            //        setDriverTrack();
+
+            //        Polyline line = mMap.addPolyline(new PolylineOptions()
+            //                .add(new LatLng(29.993058, 31.417643), new LatLng(29.996336, 31.419788),new LatLng(29.996425, 31.419915) ,new LatLng(29.996588, 31.419944))
+            //                .width(10)
+            //                .color(Color.BLUE)); how to draw polylines
+            GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+            //calling set location every 3 seconds
+            if(account==null){
+                final Handler ha=new Handler();
+                ha.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        setDriverTrack();
+                        ha.postDelayed(this, 3000);
+
+                    }
+                },3000);}
+            else{final Handler ha=new Handler();
+                ha.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        DriverTrackMove();
+                        ha.postDelayed(this, 3000);
+
+                    }
+                },3000);}
+
+
+            button.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void run() {
-                    setDriverTrack();
-                    ha.postDelayed(this, 3000);
-
-                }
-            },3000);}
-        else{final Handler ha=new Handler();
-            ha.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    DriverTrackMove();
-                    ha.postDelayed(this, 3000);
-
-                }
-            },3000);}
-
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                public void onClick(View v) {
 //                Polyline line = mMap.addPolyline(new PolylineOptions()
 //                        .add(new LatLng(29.993058, 31.417643), new LatLng(29.996336, 31.419788),new LatLng(29.996425, 31.419915) ,new LatLng(29.996588, 31.419944))
 //                        .width(10)
 //                        .color(Color.BLUE));
-                signOut();
+                    signOut();
 //                DriverTrackMove();
 
-            }
-        });
-
+                }
+            });
         }
 
-        public void setDriverTrack()
-        {
+    }
 
-            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+    public void setDriverTrack() {
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 // TODO: Consider calling
                 //    Activity#requestPermissions
                 // here to request the missing permissions, and then overriding
@@ -166,37 +213,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 // to handle the case where the user grants the permission. See the documentation
                 // for Activity#requestPermissions for more details.
                 return;
-            }
+        }
 
-            myRef.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    client.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
                         @Override
-                        public void onSuccess(Location location) {
-                            latit=location.getLatitude();
+                        public void onSuccess(Location location) { latit=location.getLatitude();
                             longit=location.getLongitude();
-        //                    String id=FirebaseAuth.getInstance().getCurrentUser().getUid();
+                            //String id=FirebaseAuth.getInstance().getCurrentUser().getUid();
                             Bundle extras = getIntent().getExtras();
                             String BusLine = extras.getString("BusLine");
                             String GoingOrReturning = extras.getString("GoingOrReturning");
                             myRef.child("Bus Lines").child(GoingOrReturning).child(BusLine).child("lat").setValue(latit);
                             myRef.child("Bus Lines").child(GoingOrReturning).child(BusLine).child("lon").setValue(longit);
-
-//                            Toast.makeText(MapsActivity.this, "Lat: "+latit+" "+"Longit: "+longit, Toast.LENGTH_SHORT).show();
-
+                            //Toast.makeText(MapsActivity.this, "Lat: "+latit+" "+"Longit: "+longit, Toast.LENGTH_SHORT).show();
                         }
                     });
                 }
 
                 @Override
                 public void onCancelled(@NonNull DatabaseError databaseError) {
-
                 }
-            });
-
-
-        }
+        });
+    }
         public void DriverTrackMove()
         {
             myRef.addValueEventListener(new ValueEventListener() {
@@ -225,7 +266,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 }
             });
-
         }
     private void signOut() {
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
